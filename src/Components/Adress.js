08ed0e6363a27fd3db1df20b922d9ea9eebc6faa
Zeeeforzain <zeeforzain@gmail.com>
2024-08-26 14:28:42 +0500

    Done with address boxes

--- src/Components/Adress.js
+++ src/Components/Adress.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import "./Adress.css";
-import Typography  from '@mui/material/Typography';
+import { Box, Typography } from "@mui/material";
+import React from "react";
+import FmdGoodOutlinedIcon from "@mui/icons-material/FmdGoodOutlined";
+import MarkunreadOutlinedIcon from "@mui/icons-material/MarkunreadOutlined";
 export default function Adress(props) {
   return (
-    <div className='adress'>
-      <Typography variant='h5'>{props.country}</Typography>
-      <div className="location">
-        <img src="" alt="location" />
-        <Typography variant='h6'>{props.adress}</Typography>
-      </div>
-      <div className="mail">
-        <img src="" alt="mail" />
-        <Typography variant='h6'>{props.Email}</Typography>
-
-      </div>
-    </div>
+    <Box
+      width={"24%"}
+      height={"25vh"}
+      display={"flex"}
+      flexDirection={"column"}
+      justifyContent={"space-around"}
+      boxShadow={"0px 2px 5px 2px #e6ebe7"}
+      padding={"15px"}
+    >
+      <Typography variant="h6" fontWeight={"700"}>
+        {props.title}
+      </Typography>
+      <Box display={"flex"} alignItems={"center"}>
+        <FmdGoodOutlinedIcon fontSize="small" color="primary" />
+        <Typography variant="caption">{props.address}</Typography>
+      </Box>
+      <Box display={"flex"} alignItems={"center"}>
+        <MarkunreadOutlinedIcon fontSize="small" color="primary" />
+        <Typography variant="caption">{props.email}</Typography>
+      </Box>
+    </Box>
   );
 }
