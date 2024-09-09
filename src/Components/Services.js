b51c153fd0d8d1bb7dbaa74ae61f3b5751d83554
Zeeeforzain <zeeforzain@gmail.com>
2024-09-09 14:37:00 +0500

    icon fixed

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -44,7 +44,7 @@ export default function Services() {
         alignItems="start"
       >
         <ServiceComponent
-          img="Images/API.png"
+          img="Images/api.png"
           title="API Integration Services"
           description="JJs IT specializes in API integration and automation services, ensuring seamless connectivity between your websites, CRM, and accounting tools."
         />
