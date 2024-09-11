0c79fb911317e1a8d3631bdd0515464d0c52e21b
Zeeeforzain <zeeforzain@gmail.com>
2024-09-11 15:17:56 +0500

    Hover

--- src/Components/ServiceComponent.js
+++ src/Components/ServiceComponent.js
@@ -11,7 +11,8 @@ export default function ServiceComponent(props) {
       sx={{
         transition: 'transform 0.3s ease-out', 
         "&:hover": {
-          transform: 'scale(1.1)',  
+          transform: 'scale(1.1)',
+          cursor:'pointer'  
         }
       }}
     >
