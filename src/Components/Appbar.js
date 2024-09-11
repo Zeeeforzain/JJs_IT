e05683304f5094b71451c1a63389030b54999739
Zeeeforzain <zeeforzain@gmail.com>
2024-09-11 15:56:39 +0500

    Menu fixed

--- src/Components/Appbar.js
+++ src/Components/Appbar.js
@@ -22,31 +22,31 @@ const navItems = [
   {
     label: (
       <BoxComponent
-      sx={{
-        height:{xs:'auto', sm:'5vh', md:'6.5vh', lg:'7.5vh', xl:'7.5vh'},
-        width:{xs:'auto', sm:'100px', md:'125px', lg:'135px', xl:'165px'}
-      }}>
-      <ButtonComponent
-      variant="contained"
-      color="var(--primary)"
-      sx={{
-        textTransform: "none",
-         fontFamily:'var(--main)',
-        padding: '10px 5px',
-        borderRadius:'8px',
-        width: {xs:'100px', md:'120px',lg:'130px', xl:'160px'}, 
-        height: {xs:'5vh', md:'6vh',lg:'7vh',xl:'7vh'}, 
-        transition: "width 0.3s ease-out, height 0.3s ease-out", 
-        "&:hover": {
-          transform: 'scale(1.1)',
-          cursor:'pointer',
-          transition:'0.5s ease-out'
-        }
-      }}
-    >
-      Contact Us
-    </ButtonComponent></BoxComponent>
-    
+        sx={{
+          height:{xs:'auto', sm:'5vh', md:'6.5vh', lg:'7.5vh', xl:'7.5vh'},
+          width:{xs:'auto', sm:'100px', md:'125px', lg:'135px', xl:'165px'}
+        }}>
+        <ButtonComponent
+          variant="contained"
+          color="var(--primary)"
+          sx={{
+            textTransform: "none",
+            fontFamily:'var(--main)',
+            padding: '10px 5px',
+            borderRadius:'8px',
+            width: {xs:'100px', md:'120px', lg:'130px', xl:'160px'}, 
+            height: {xs:'5vh', md:'6vh', lg:'7vh', xl:'7vh'}, 
+            transition: "width 0.3s ease-out, height 0.3s ease-out", 
+            "&:hover": {
+              transform: 'scale(1.1)',
+              cursor:'pointer',
+              transition:'0.5s ease-out'
+            }
+          }}
+        >
+          Contact Us
+        </ButtonComponent>
+      </BoxComponent>
     ),
     path: "/ContactForm",
   },
@@ -60,7 +60,6 @@ function Appbar(props) {
 
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
-   
   };
 
   const handleNavClick = (path) => {
@@ -88,8 +87,7 @@ function Appbar(props) {
 
   const drawer = (
     <BoxComponent onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-      <BoxComponent margin={"10px 0px 10px 10px"}
-      >
+      <BoxComponent margin={"10px 0px 10px 10px"}>
         <Title img="Images/jjs_it.png" width='50px' />
       </BoxComponent>
       <Divider />
@@ -102,7 +100,7 @@ function Appbar(props) {
               cursor: item.path ? "pointer" : "default",
               justifyContent: "left",
               textAlign: "center",
-              width: "100%" ,
+              width: "100%",
               fontFamily:'var(--main)',
             }}
           >
@@ -116,7 +114,7 @@ function Appbar(props) {
   const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <BoxComponent sx={{ display: "flex", justifyContent: "center", alignItems:'center'}}>
+    <BoxComponent sx={{ display: "flex", justifyContent: "center", alignItems:'center' }}>
       <CssBaseline />
       <AppBar
         component="nav"
@@ -132,35 +130,27 @@ function Appbar(props) {
           justifyContent: "center",
           boxShadow: scrolling ? 1 : 0,
           transition: "background-color 0.5s ease, color 0.5s ease",
-           padding:{xs:'0px', sm:'0px', md:'0px', lg:'0px', xl:'0px 150px 0px 0px'}
+          padding: { xs: '0px', sm: '0px', md: '0px', lg: '0px', xl: '0px 150px 0px 0px' }
         }}
       >
-        <Toolbar
-          sx={{
-            justifyContent: "space-between",
-            flexDirection: { xs: "row-reverse", sm:'row', md: "row" },
-          }}
-        >
+        <Toolbar sx={{ justifyContent: "space-between", flexDirection: { xs: "row-reverse", sm: 'row-reverse', md: "row" } }}>
+         
           <IconButton
             color="inherit"
             aria-label="open drawer"
             edge="end"
             onClick={handleDrawerToggle}
-            sx={{ mr: 0, display: { xs: "block", sm: "none" } }}
+            sx={{ mr: 0, display: { xs: "block", sm: "block", md: "none" } }}
           >
             <MenuIcon />
           </IconButton>
-          <BoxComponent margin={{xs:'10px 0px 10px 0px',md:"10px 0px 10px 50px"}}>
-            <Title img="Images/jjs_it.png" width='50px'
-              sx={{
-                display: { xs: "block", sm: "block" },
-                flexGrow: 0,
-                ml: { xs: 0, sm: 2 },
-                textAlign: { xs: "left", sm: "inherit" },
-              }}
-            />
+
+          <BoxComponent margin={{ xs: '10px 0px 10px 0px', md: "10px 0px 10px 50px" }}>
+            <Title img="Images/jjs_it.png" width='50px' />
           </BoxComponent>
-          <BoxComponent sx={{ display: { xs: "none", sm: "block" }, width: "auto" }}>
+
+        
+          <BoxComponent sx={{ display: { xs: "none", sm: "none", md: "block" }, width: "auto" }}>
             {navItems.map((item, index) => (
               <ButtonComponent
                 key={index}
@@ -168,10 +158,10 @@ function Appbar(props) {
                 sx={{
                   color: scrolling ? "var(--dull)" : "var(--light)",
                   mx: 2,
-                  fontSize:'15px',
+                  fontSize: '15px',
                   fontWeight: "500",
-                  textTransform:'none',
-                   fontFamily:'var(--main)',
+                  textTransform: 'none',
+                  fontFamily: 'var(--main)',
                 }}
               >
                 {item.label}
@@ -180,17 +170,17 @@ function Appbar(props) {
           </BoxComponent>
         </Toolbar>
       </AppBar>
+
+      {/* Drawer for xs and sm screens */}
       <nav>
         <Drawer
           container={container}
           variant="temporary"
           open={mobileOpen}
           onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, 
-          }}
+          ModalProps={{ keepMounted: true }}
           sx={{
-            display: { xs: "block", sm: "none" },
+            display: { xs: "block", sm: "block", md: "none" }, // Drawer for xs and sm
             "& .MuiDrawer-paper": {
               boxSizing: "border-box",
               width: drawerWidth,
@@ -200,6 +190,8 @@ function Appbar(props) {
           {drawer}
         </Drawer>
       </nav>
+
+      {/* Placeholder for the rest of the page content */}
       <BoxComponent component="main" sx={{ p: 3 }}>
         <Toolbar />
       </BoxComponent>
